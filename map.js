6d1bcce8172567f9346af4320ee1800103f87339
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2021-10-28 23:10:10 +0000

    added forEach loop to iterate through array

--- map.js
+++ map.js
@@ -1,6 +1,10 @@
 const words = ["ground", "control", "to", "major", "tom"];
-const map = function(array,callback) {
+const map = function(arr,callback) {
 const newArray = [];
+arr.forEach((value) => {
+  newArray.push(value);
+  return newArray;})
+  console.log(newArray);
 };
 const results1 = map(words, word => word[0]);
 console.log(results1);
