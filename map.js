e72092a9be2847ce7ecec8136ab5ffd3b36ed7f3
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2021-10-28 22:54:11 +0000

    initialize map function

---
+++ map.js
@@ -0,0 +1,6 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array,callback) {
+const newArray = [];
+};
+const results1 = map(words, word => word[0]);
+console.log(results1);
