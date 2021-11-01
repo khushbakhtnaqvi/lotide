9dc6ed47aed7c418d630e66256dfa1cef50728e9
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2021-11-01 19:27:41 -0400

    refactor middle and create test file

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  const arr = (array1.length >= array2.length ? array1 : array2);
-  for (let i in arr) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  let assertion;
-  if (eqArrays(actual, expected) === true) {
-    assertion = console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
-  } else {
-    assertion = console.log(`🛑🛑🛑 Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
-  }
-  return assertion;
-};
-
 const middle = function(array) {
   let result = [];
   let index = 0;
@@ -33,11 +13,4 @@ const middle = function(array) {
   return result;
 };
 
-assertArraysEqual(middle([1]),[]); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-
-assertArraysEqual(middle([1, 2, 3]),[2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]); // => [3, 4]
+module.exports = middle;
