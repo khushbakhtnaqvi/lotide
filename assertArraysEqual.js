70ec87c0d789704a811940291f9c0570feb708a4
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2021-11-01 16:59:01 -0400

    refactor assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,4 @@
-const eqArrays = function(array1, array2) {
-  const arr = (array1.length >= array2.length ? array1 : array2)
-  for (let i in arr) {
-     if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-      return true;
-}
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(actual, expected) {
   let assertion;
